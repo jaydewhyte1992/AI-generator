4728cf7a0dd6a10bcc26688a38190145cd6e62d3
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-04-22 00:24:40 +1000

    added API intergration

--- script.js
+++ script.js
@@ -1,11 +1,23 @@
-function generateTips(event) {
-  event.preventDefault();
-
+function displayTips(response) {
   new Typewriter("#tips", {
-    strings: "Travel tips here..",
+    strings: response.data.answer,
     autoStart: true,
     cursor: "✈️",
+    delay: 1,
   });
 }
+
+function generateTips(event) {
+  event.preventDefault();
+  let userRequest = document.querySelector("#user-input").value;
+  let apiKey = "dec30ab936f6fe43ot4b0dead25ade10";
+  let prompt = `Generate travel tips about ${userRequest}`;
+  let context =
+    "User instructions: You are a witty travel expert who loves to provide unique travel tips. Your mission is to generate a list of 5 top travel tips that are both important and humorous. Please provide tips in basic HTML. Make sure to follow the user instructions. Before listing the tips, the heading should be, 'Top five travel tips for'. Please keep answers short and witty, make sure each point is on a new line and keep the mood light and fun!";
+  let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
+
+  axios.get(apiURL).then(displayTips);
+}
+
 let travelFormElement = document.querySelector("#travel-tips-form");
 travelFormElement.addEventListener("submit", generateTips);
