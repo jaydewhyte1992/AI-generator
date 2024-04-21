fa3871cf4b24f36b53d8e773f5ee28d5be8926d8
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-04-21 22:48:08 +1000

    added basic JS and fixed some HTML id's

--- script.js
+++ script.js
@@ -1,1 +1,11 @@
+function generateTips(event) {
+  event.preventDefault();
 
+  new Typewriter("#tips", {
+    strings: "Travel tips here..",
+    autoStart: true,
+    cursor: "✈️",
+  });
+}
+let travelFormElement = document.querySelector("#travel-tips-form");
+travelFormElement.addEventListener("submit", generateTips);
