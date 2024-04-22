5d5d7b2da393c90dd8ac30014b4091af15350ec7
jaydewhyte1992 <135990890+jaydewhyte1992@users.noreply.github.com>
2024-04-22 23:55:56 +1000

    finishing up my CSS, fixing the positioning of footer

--- script.js
+++ script.js
@@ -1,5 +1,4 @@
 function displayTips(response) {
-  console.log("Generated travel tips:", response.data.answer);
   new Typewriter("#tips", {
     strings: response.data.answer,
     autoStart: true,
@@ -14,7 +13,7 @@ function generateTips(event) {
   let apiKey = "dec30ab936f6fe43ot4b0dead25ade10";
   let prompt = `Generate travel tips about ${userRequest}`;
   let context =
-    "User instructions: As a witty travel expert, craft a list of 5 humorous travel tips in basic HTML format. Begin with the heading Top Five Travel Tips for and maintain a light-hearted tone. Avoid numbering, use bullets, and ensure each tip is concise and witty.";
+    "User instructions: As a witty travel expert, craft a list of 5 humorous travel tips in basic HTML format. Always begin with the heading Top Five Travel Tips for and maintain a light-hearted tone. Avoid numbering, use bullets, and ensure each tip is concise and witty.";
   let apiURL = `https://api.shecodes.io/ai/v1/generate?prompt=${prompt}&context=${context}&key=${apiKey}`;
 
   let inputElement = document.querySelector("#tips");
@@ -25,3 +24,7 @@ function generateTips(event) {
 
 let travelFormElement = document.querySelector("#travel-tips-form");
 travelFormElement.addEventListener("submit", generateTips);
+
+window.onload = function () {
+  document.querySelector("#user-input").value = "";
+};
